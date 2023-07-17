c721d71148688ccf444d246af7acfd59eeace447
titustum <tituskiptanuitum@gmail.com>
2023-07-18 00:33:06 +0300

    Initial commit

--- NumberToWords.php
+++ NumberToWords.php
@@ -41,20 +41,130 @@ class NumberToWords {
     }
 
     public function getWords(){
-        if( array_key_exists($this->number, $this->definedNumbers) ) {
-            return $this->definedNumbers[$this->number];
-        }
+        // if( array_key_exists($this->number, $this->definedNumbers) ) {
+        //     return $this->findDefined($this->number);
+        // }
         if($this->number < 100){
-            $num1 = floor($this->number/10);
-            $num2 = $this->number - ($num1*10);
-            return $this->definedNumbers[$this->number - $num2]. " ".$this->definedNumbers[$num2];
+            return $this->findBelow100($this->number);
+        }
+        if($this->number < 1000){
+            return $this->findBelow1000($this->number);
+        }
+        if($this->number < 10000){
+            return $this->findBelow10k($this->number);
+        }
+
+        if($this->number < 100000){
+            return $this->findBelow100k($this->number);
+        }
+        if($this->number < 1000000){
+            return $this->findBelow1Million($this->number);
+        }
+        if($this->number < 1000000000){
+            return $this->findBelowBillion($this->number);
+        }
+        if($this->number < 1000000000000){
+            return $this->findBelowTrillion($this->number);
         }
         return number_format($this->number);
+    }
+
+    private function findDefined($input_num){
+        return $this->definedNumbers[$input_num];
+    }
+    private function findBelow100($input_num){
+        if($input_num < 20) {
+            return $this->findDefined($input_num);
+        }
+        $num1 = floor($input_num/10); 
+        $num2 = $input_num - ($num1*10);
+        if ($num2 < 1) {
+            return $this->definedNumbers[$input_num - $num2];
+        } 
+        return $this->definedNumbers[$input_num - $num2]. " ".$this->definedNumbers[$num2];
+    }
+    private function findBelow1000($input_num){
+        if($this->number < 100){
+            return $this->findBelow100($this->number);
+        }
+        $num1 = floor($input_num/100);
+        $num2 = $input_num - ($num1*100);
+
+        $words = "";
+        if ($num1 > 0) {
+            $words .= $this->definedNumbers[$num1]." hundred " ;
+        }
+        $words .= "and ". $this->findBelow100($num2);
+        return $words;
+    }
+
+    private function findBelow10k($input_num){
+
+        if($this->number < 1000){
+            return $this->findBelow1000($this->number);
+        }
+        $num1 = floor($input_num/1000);
+        $words = '';
+        $num2 = $input_num - ($num1*1000);
+        if($num1 > 0){
+            $words = $this->definedNumbers[$num1] . " thousand,";
+        } 
+        $words  .= $this->findBelow1000($num2);
+        return $words;
+    }
+
+    private function findBelow100k($input_num){
+        $num1 = floor($input_num/1000);
+        $num2 = $input_num - ($num1*1000);
+
+        $words = $this->findBelow100($num1);
+        $words  .=  " thousand, " . $this->findBelow10k($num2);
+        return $words;
+    }
+
+    private function findBelow1Million($input_num){
+        $num1 = floor($input_num/1000);
+        $num2 = $input_num - ($num1*1000);
+
+        $words = $this->findBelow1000($num1);
+        $words  .=  " thousand, " . $this->findBelow1000($num2);
+        return $words;
+    }
+
+    private function findBelowBillion($input_num){
+        $num1 = floor($input_num/1000000);
+        $num2 = $input_num - ($num1*1000000);
+        if ($num1 < 20) {
+            return $this->definedNumbers[$num1]  . " million, " . $this->findBelow1Million($num2);
+        }
+        if ($num1 < 100) {
+            return $this->findBelow100($num1)  . " million, " . $this->findBelow1Million($num2);
+        }
+        return $this->findBelow1000($num1)  . " million, " . $this->findBelow1Million($num2);
+    }
+
+    private function findBelowTrillion($input_num){
+        $num1 = floor($input_num/1000000000);
+        $num2 = $input_num - ($num1*1000000000);
+        if ($num1 < 20) {
+            return $this->definedNumbers[$num1]  . " billion, " . $this->findBelowBillion($num2);
+        }
+        if ($num1 < 100) {
+            return $this->findBelow100($num1)  . " billion, " . $this->findBelowBillion($num2);
+        }
+        return $this->findBelow1000($num1)  . " billion, " . $this->findBelowBillion($num2);
     }
 
 }
 
 
-$number = 67;
-$num2Words = new NumberToWords($number);
-echo $num2Words->getWords();
+// $number = 800000;
+// echo number_format($number).'<br>';
+// $num2Words = new NumberToWords($number);
+// echo $num2Words->getWords();
+
+for ($i=0; $i < 100; $i++) {
+    $num = random_int(10, 10000000000);
+    $num2Words = new NumberToWords($num);
+    echo number_format($num)."=>". $num2Words->getWords() ."<br>";
+}
